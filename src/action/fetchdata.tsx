2508c6486bc2abcd067f5caf5b26efbeb5914e06
@1stBoss <dduoc2002@gmail.com>
2024-11-19 17:08:00 +0700

    5

--- src/action/fetchdata.tsx
+++ src/action/fetchdata.tsx
@@ -3,8 +3,8 @@
 
 export const fetchData = async () => {
     try {
-      // const response = await fetch("http://27.72.246.67:8710/api/data1");
-      const response = await fetch("http://127.0.0.1:8710/api/data1");
+      const response = await fetch("http://27.72.246.67:8710/api/data1");
+      // const response = await fetch("http://127.0.0.1:8710/api/data1");
       if (!response.ok) throw new Error("Network response was not ok");
       const result = await response.json();
       return result;

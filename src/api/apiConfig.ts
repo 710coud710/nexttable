90dbd35c3c7e2abbf233e8a72d3a183128236878
@1stBoss <dduoc2002@gmail.com>
2024-12-02 17:03:45 +0700

    hdds

--- src/api/apiConfig.ts
+++ src/api/apiConfig.ts
@@ -1,5 +1,6 @@
 export const API_ENDPOINTS = {
-    BASE_URL: "http://127.0.0.1:8710",
+    // BASE_URL: "http://127.0.0.1:8710",
+    BASE_URL: "http://27.72.246.67:8710",
     UPLOAD: '/api/upload',
     DATA: '/api/data',
     DATA1: '/api/data1',
